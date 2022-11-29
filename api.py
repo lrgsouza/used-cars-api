f8bd8ecafc876c33757219d2e6abb002205379c3
Airton Gabriel <55886112+airtong@users.noreply.github.com>
2022-11-29 20:14:52 -0300

    API integration, using Flask

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+# Init app
+app = Flask(__name__)
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+# Flask maps HTTP requests to Python functions.
+# The process of mapping URLs to functions is called routing.
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Used Cars Graph</h1><p>Vehicle Data Query</p>"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found</p>", 404
+
+# A method that runs the application server.
+if __name__ == "__main__":
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(debug=False, threaded=True, port=5000)
