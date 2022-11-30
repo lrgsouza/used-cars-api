00f201b0b5b3fbb53474078f7333647e1a331d9d
Airton Gabriel <55886112+airtong@users.noreply.github.com>
2022-11-30 12:29:24 -0300

    Better templates. Pattern defined

--- api.py
+++ api.py
@@ -1,12 +1,9 @@
 
-from flask import Flask, render_template, request, redirect
-
-import jsonpickle as jp
+from flask import Flask, request, redirect, url_for, render_template
 
 from api_files.api_html_page import app as html_page
 from api_files.api_json_page import app as json_page
 from api_files.api_config_page import app as config_page
-from oop.car import Car
 
 # Init app
 
@@ -15,6 +12,10 @@ app = Flask(__name__)
 app.register_blueprint(json_page)
 app.register_blueprint(html_page)
 app.register_blueprint(config_page)
+
+@app.route('/test', methods=['GET', 'POST'])
+def index():
+    return render_template('tables.html')
 
 
 # Flask maps HTTP requests to Python functions.
