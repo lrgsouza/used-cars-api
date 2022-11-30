c1a892e6ef4a6af0b5cf70971457febd8308e627
Airton Gabriel <55886112+airtong@users.noreply.github.com>
2022-11-30 11:57:46 -0300

    Separate api functions in different files part 3

--- api_files/api_html_page.py
+++ api_files/api_html_page.py
@@ -6,6 +6,10 @@ from oop.car import Car
 app = Blueprint('html_page', __name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
 # Flask maps HTTP requests to Python functions.
 @app.route('/home', methods=['GET', 'POST'])
 def index():
@@ -23,7 +27,22 @@ def index():
         return redirect(url_for('index'))
 
     cars = Car().readPlate('VCZ8Z16')
-    return render_template('index.html', cars=cars)
+    return render_template('index.html', cars=cars)@app.route('/register', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        car = Car()
+        car.brand = request.form['brand']
+        car.model = request.form['model']
+        car.year = request.form['year']
+        car.fuel = request.form['fuel']
+        car.km = request.form['km']
+        car.engine = request.form['engine']
+        car.plate = request.form['plate']
+        car.sold = False
+        car.create()
+        return redirect(url_for('/'))
+    return render_template('register.html')
+
 
 
 @app.route('/model/<model>', methods=['GET'])
@@ -38,12 +57,20 @@ def byplate(model):
 @app.route('/filter', methods=['GET', 'POST'])
 def filter():
     if request.method == 'POST':
+        car_dict = {}
         model = request.form['model']
-        cars = Car().readModel(model)
+        brand = request.form['brand']
+        if model:
+            car_dict['model'] = model
+        if brand:
+            car_dict['brand'] = brand
+        # cars = Car().readModel(model)
+        print(car_dict)
+        cars = Car().readByDict(car_dict)
         carros = []
         for car in cars:
             carros.append(car)
-        return render_template('filter.html', cars=carros)
+        return render_template('tableTemplate.html', cars=carros, model=model, len=len(carros))
     return render_template('filter.html')
 
 
@@ -51,6 +78,6 @@ def filter():
 def delete():
     if request.method == 'POST':
         plate = request.form['plate']
-        Car().delete(plate)
-        return redirect(url_for('delete'))
-    return render_template('delete.html')
+        res = Car().delete(plate)
+        return render_template('delete.html',num=res)
+    return render_template('delete.html',num=-1)
